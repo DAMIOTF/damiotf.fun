cdec0a60feb2f7d412793a28765e4865792e7b6d
DAMIOTF <damiotv1@gmail.com>
2024-06-26 13:10:08 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+var pisane = new Typed('#pisane', {
+    strings: ['3D Designer', 'Python C# HTML dev', 'More coming soon...'],
+    typeSpeed: 50,
+    startDelay: 1200,
+    showCursor: true,
+    cursorChar: '_',
+    loop: true,
+    backDelay: 2000,
+    backSpeed: 50,
+});
